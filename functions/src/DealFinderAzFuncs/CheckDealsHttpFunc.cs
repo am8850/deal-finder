c4ce2b3f781139ba1227a235db06ae35d0394ce8
Alex Morales <am8850mia@gmail.com>
2021-06-19 20:32:57 -0400

    Added bootstrap to emails

--- functions/src/DealFinderAzFuncs/CheckDealsHttpFunc.cs
+++ functions/src/DealFinderAzFuncs/CheckDealsHttpFunc.cs
@@ -138,29 +138,30 @@ namespace DealFinderAzFuncs
             if (results.Count > 0)
             {
                 sb.Append("<html><head><title></title>");
-                sb.Append("<style>.btn { " +
-                "display: inline-block; " +
-                "font-weight: 400; " +
-                "line-height: 1.5; " +
-                "color:#212529; " +
-                "text-align: center; " +
-                "text-decoration: none; " +
-                "vertical-align: middle; " +
-                "cursor: pointer; " +
-                "-webkit-user-select: none; " +
-                "-moz-user-select: none; " +
-                "user-select: none; " +
-                "background-color: transparent; " +
-                "border: 1px solid transparent; " +
-                "padding: 0.375rem 0.75rem; " +
-                "font-size: 1rem; " +
-                "border-radius: 0.25rem; " +
-                "transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;}" +
-                ".btn-primary {" +
-                "color: #fff;" +
-                "background-color: #0d6efd;" +
-                "border-color: #0d6efd;}</style>");
-                sb.Append("</head><body><div style=\"text-aling=center;\"><table>");
+                sb.Append("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css\" integrity=\"undefined\" crossorigin=\"anonymous\">");
+                // sb.Append("<style>.btn { " +
+                // "display: inline-block; " +
+                // "font-weight: 400; " +
+                // "line-height: 1.5; " +
+                // "color:#212529; " +
+                // "text-align: center; " +
+                // "text-decoration: none; " +
+                // "vertical-align: middle; " +
+                // "cursor: pointer; " +
+                // "-webkit-user-select: none; " +
+                // "-moz-user-select: none; " +
+                // "user-select: none; " +
+                // "background-color: transparent; " +
+                // "border: 1px solid transparent; " +
+                // "padding: 0.375rem 0.75rem; " +
+                // "font-size: 1rem; " +
+                // "border-radius: 0.25rem; " +
+                // "transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;}" +
+                // ".btn-primary {" +
+                // "color: #fff;" +
+                // "background-color: #0d6efd;" +
+                // "border-color: #0d6efd;}</style>");
+                sb.Append("</head><body><div><table>");
                 foreach (var deal in results)
                 {
                     sb.Append("<tr><td align='center' style='padding:3px;'>");
